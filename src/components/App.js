581ea29b13cb4161c37dcec31c8fc3e4f9fbe77a
fastboy <giagiang200@gmail.com>
2023-07-14 16:30:23 +0700

    updateUi/future

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,16 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contacts  = [
-    {
-      id :  "1",
-      "name " : "ricciardo",
-      "email" : "ricciardo@gmail.com"
-    },
-    {
-      id :  "2",
-      "name " : "Cristiano Ronaldo ",
-      "email" : "siuuuuuuuo@gmail.com"
-    },
-    {
-      id :  "3",
-      "name " : "ric g",
-      "email" : "ric g@gmail.com"
-    }
-
-  ]
+  const [contacts, setContacts] = useState([])
   return (
     <div className="ui container">
       <Header />
       <AddContact />
-      <ContactList  Contacts = {contacts}/>
+      <ContactList contacts={contacts} />
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,21 +1,14 @@
 import React from "react";
+import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
   console.log(props);
-  const renderContactList = props.contact.map((contact) => {
-    return (
-      <div className="item">
-        <div className="content">
-          <div className="header">{contact.name}</div>
-          <div> {contact.email}</div>
-        </div>
-        <i className="trash aliternate outline icon "></i>
-      </div>
-    );
+  const renderContactList = props.contacts.map((contact) => {
+    return <ContactCard contacts={contact}/>;
   });
-  return <div className="ui called list"> Contact List</div>;
+  return <div className="ui called list">{renderContactList} </div>;
 };
 
 export default ContactList;
 
-// rafce
+// rafce  
