ff7b781e9828e84d24d1922f3ed69759ea436271
fastboy <giagiang200@gmail.com>
2023-09-26 10:40:22 +0700

    searchContact-UPDATE-CRUD

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import api from "../api/contacts";
-import retrieveContacts from "../api/contacts";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
@@ -11,58 +9,13 @@ import EditContact from "./EditContact";
 import { ContactsCrudContextProvider } from "../context/ContactsCrudContext";
 
 function App() {
-  const LOCAL_STORAGE_KEY = "contacts";
-  const [contacts, setContacts] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchResult, setSearchResult] = useState([]);
-
- 
-  const searchHandler = (searchTerm) => {
-    setSearchTerm(searchTerm);
-    if (searchTerm !== "") {
-      const newContactList = contacts.filter((contact) => {
-        return Object.values(contact)
-          .join(" ")
-          .toLowerCase()
-          .includes(searchTerm.toLowerCase());
-      });
-      setSearchResult(newContactList);
-    } else {
-      setSearchResult(contacts);
-    }
-  };
-
-  useEffect(() => {
-    // console.log("use effect get all contact run!!");
-    const getAllContacts = async () => {
-      const allContacts = await retrieveContacts();
-      if (allContacts) setContacts(allContacts);
-    };
-    getAllContacts();
-  }, []);
-
-  useEffect(() => {}, [contacts]);
-
   return (
     <div className="ui container">
       {/* <Router> */}
       <Header />
       <ContactsCrudContextProvider>
         <Routes>
-          <Route
-            path="/"
-            exact
-            element={<ContactList />}
-            // render={(props) => (
-            //   <ContactList
-            //     {...props}
-            //     contacts={searchTerm.length < 1 ? contacts : searchResult}
-            //     getContactId={removeContactHandler}
-            //     term = {searchTerm}
-            //     searchKeyword ={ searchHandler}
-            //   />
-            // )}
-          />
+          <Route path="/" exact element={<ContactList />} />
           <Route path="/add" element={<AddContact />} />
           <Route path="/edit" element={<EditContact />} />
           <Route path="/contact/:id" element={<ContactDetail />} />
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,34 +1,42 @@
-import React, {useRef,useEffect} from "react"; 
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import ContactCard from "./ContactCard";
 import { useContactsCrud } from "../context/ContactsCrudContext";
 
-
 const ContactList = (props) => {
-  const{contacts, retrieveContacts} = useContactsCrud();
-  const inputEl = useRef("");
-  const deleteConactHandler = (id )=>{
-    props.getContactId();
-  }
+  const {
+    contacts,
+    retrieveContacts,
+    searchTerm,
+    searchResults,
+    searchHandler,
+  } = useContactsCrud();
+  // const inputEl = useRef("");
+
   // const deleteContactHandler = (id) => {
   //   props.getContactId(id);
   // };
-
-  useEffect(()=>{
+  console.log("searchREsult", searchResults);
+  useEffect(() => {
     retrieveContacts();
-  },[]);
-  
-  const renderContactList = contacts.map((contact) => {
-    return (
-      <ContactCard
-        contact={contact}
-        key={contact.id}
-      />
-    );
-  });
-  const getSearchTerm = () => {
-    props.searchKeyword(inputEl.current.value);
+  }, []);
+  const foundedContacts = searchTerm.length < 1 ? contacts : searchResults;
+  const renderContactList =
+    foundedContacts &&
+    foundedContacts.map((contact) => {
+      return <ContactCard contact={contact} key={contact.id} />;
+    });
+  // if (searchResults === undefined) {
+  //   console.log("bị undefiend")
+  // } else {
+  //   console.log("k bị ")
+  // }
+  const onUserSearch = (e) => {
+    debugger;
+    searchHandler(e.target.value);
   };
+  debugger;
+
   return (
     <div className="main">
       <h2>
@@ -40,12 +48,12 @@ const ContactList = (props) => {
       <div className="ui search">
         <div className="ui icon input">
           <input
-            ref = {inputEl}
+            // ref={inputEl}
             type="text"
             placeholder="Search Contacts"
             className="prompt"
-            value={props.term}
-            onChange={getSearchTerm}
+            value={searchTerm}
+            onChange={(e) => onUserSearch(e)}
           />
           <i className="search icon"></i>
         </div>
